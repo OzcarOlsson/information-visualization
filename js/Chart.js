8dc8e0f40ff765ed95e4a87473a0deb849942f85
HenrikRosander <34091618+HenrikRosander@users.noreply.github.com>
2021-02-25 14:14:07 +0100

    Now calculates all avarage temperature deviations of the countries

--- js/Chart.js
+++ js/Chart.js
@@ -1,16 +1,68 @@
 function chart(data, arr){
+
     const data2 = parseData(data)
 
     function parseData(d) {
       let temp = []
-      for (let i = 0; i < d.length; i++) {
-        if (d[i].country_name == 'Sweden') {
-          temp.push({
-            dt: new Date(d[i].year),
-            avgTemp: +d[i].value,
-          })
-        }
+
+      for(let j = 0; j < arr.length; j++){
+        for (let i = 0; i < d.length; i++) {
+          
+            if (d[i].country_name == arr[j]) {
+                temp.push({
+                  name: d[i].country_name,
+                  dt: new Date(d[i].year),
+                  avgTemp: +d[i].value,
+                })
+              }
+          }
+        
       }
       return temp
     }
+
+    console.log(data2);
+
+    //y is an array that holds all the avarage deviations of the countries temperatures.
+    let y = calcAvarage(data2);
+
+    function calcAvarage(d, years = d.length){
+
+        let avg = [];
+        let dummy = 0;
+        
+        for(let j = 0; j < arr.length; j++){
+            let counter = 0;
+            for (let i = (d.length-years); i < d.length; i++) {
+                if(d[i].name == arr[j]){
+                    dummy += d[i].avgTemp;
+                    counter++;
+                }
+            }
+            
+            avg[j] = dummy /  counter;
+        }
+        return avg;
+    }
+
+
+    let a = document.getElementById("bottomContainer");
+
+    a.textContent = ("Avarage temperature deviation for " + arr[0] + " is: " + parseFloat(y).toFixed(3) + "°C")
+
+
+    
+
+
+    //Calculates the indices that will be the n amount of highest deviations (for a time period k)
+    function huh(){
+
+    }
+
+    for (let i = 1; i < 10; i++) {
+        a.textContent += ("\n Avarage temperature deviation for " + arr[i] + " is: " + parseFloat(y[i]).toFixed(3) + "°C")
+    }
+    
+
+    // console.log(data2);
 }
--- js/main.js
+++ js/main.js
@@ -4,64 +4,8 @@ const ls = "hej";
 let k;
 
 d3.csv('/data/temp_data.csv').then((data) => {
-    // console.log(data)
     let countryArr = data.map((item) => item.country_name).filter((value, index, self) => self.indexOf(value) === index)
-   
 
     let a; 
     a = new chart(data, countryArr);
-    console.log(countryArr)
 })
-
-
-// d3.csv("/data/temp_data.csv", function(d) {
-//     return {
-//         country_name : d.country_name,
-//         year : d.year,
-//         value : storeTemp(d.value),
-//         country_code : d.country_code,
-//     };
-//   }).then(function(data) {
-//      calcAvarage(data.temperatures);
-//      console.log(data[0]);
-
-    
-
-
-//      k = new chart(data, arr)
-//   });
-// console.log(ls);
-
-// var box = document.getElementById("bottomContainer");
-// box.textContent += " hej";
-
-// function calcAvarage(data){
-
-//     let o = 0;
-
-//     for (let i = 0; i < data.temperatures.length; i++) {
-//         // console.log(data.temperatures[i])
-//         o = parseFloat(data.temperatures[i]);
-//         // console.log(o);
-//     }
-//     o = o / data.temperatures.length;
-//     // console.log(o);
-    
-// }
-
-
-// function storeTemp(data){
-//     let arr = [];
-//     let Y = 1961;
-//     let ind = "Y" + Y.toString(); 
-//     for (let i = 0; i < 58; i++) {
-        
-//         if(data[ind]!=null){
-//             arr.push(data[ind])
-//             console.log(data[ind])
-//         }
-//         Y++;
-//         ind = "Y" + Y.toString(); 
-//     }
-//     return arr;
-// }
