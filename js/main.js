73571f5150b456749962606d00906171e582c34f
HenrikRosander <34091618+HenrikRosander@users.noreply.github.com>
2021-02-25 12:08:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,54 +14,54 @@ d3.csv('/data/temp_data.csv').then((data) => {
 })
 
 
-d3.csv("/data/temp_data.csv", function(d) {
-    return {
-        country_name : d.country_name,
-        year : d.year,
-        value : storeTemp(d.value),
-        country_code : d.country_code,
-    };
-  }).then(function(data) {
-     calcAvarage(data.temperatures);
-     console.log(data[0]);
+// d3.csv("/data/temp_data.csv", function(d) {
+//     return {
+//         country_name : d.country_name,
+//         year : d.year,
+//         value : storeTemp(d.value),
+//         country_code : d.country_code,
+//     };
+//   }).then(function(data) {
+//      calcAvarage(data.temperatures);
+//      console.log(data[0]);
 
     
 
 
-     k = new chart(data, arr)
-  });
-console.log(ls);
+//      k = new chart(data, arr)
+//   });
+// console.log(ls);
 
-var box = document.getElementById("bottomContainer");
-box.textContent += " hej";
+// var box = document.getElementById("bottomContainer");
+// box.textContent += " hej";
 
-function calcAvarage(data){
+// function calcAvarage(data){
 
-    let o = 0;
+//     let o = 0;
 
-    for (let i = 0; i < data.temperatures.length; i++) {
-        // console.log(data.temperatures[i])
-        o = parseFloat(data.temperatures[i]);
-        // console.log(o);
-    }
-    o = o / data.temperatures.length;
-    // console.log(o);
+//     for (let i = 0; i < data.temperatures.length; i++) {
+//         // console.log(data.temperatures[i])
+//         o = parseFloat(data.temperatures[i]);
+//         // console.log(o);
+//     }
+//     o = o / data.temperatures.length;
+//     // console.log(o);
     
-}
+// }
 
 
-function storeTemp(data){
-    let arr = [];
-    let Y = 1961;
-    let ind = "Y" + Y.toString(); 
-    for (let i = 0; i < 58; i++) {
+// function storeTemp(data){
+//     let arr = [];
+//     let Y = 1961;
+//     let ind = "Y" + Y.toString(); 
+//     for (let i = 0; i < 58; i++) {
         
-        if(data[ind]!=null){
-            arr.push(data[ind])
-            console.log(data[ind])
-        }
-        Y++;
-        ind = "Y" + Y.toString(); 
-    }
-    return arr;
-}
+//         if(data[ind]!=null){
+//             arr.push(data[ind])
+//             console.log(data[ind])
+//         }
+//         Y++;
+//         ind = "Y" + Y.toString(); 
+//     }
+//     return arr;
+// }
